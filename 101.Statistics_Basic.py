1e0e5941d1733d4082deaa701537d24ac70c03c0
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-07 16:22:50 +0100

    Update 101.Statistics_Basic.py

--- 101.Statistics_Basic.py
+++ 101.Statistics_Basic.py
@@ -1,9 +1,29 @@
 # Supponiamo di avere dei dati ordinati in un array
-speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+data = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
-x = np.mean(speed)
-y = np.median(speed)
-z = np.mode(speed)
+# MEAN, MEDIAN, MODE
+mean = np.mean(data)
+median = np.median(data)
+mode = st.mode(data)
+
+# STANDARD DEVIATION AND VARIANCE
+std = np.std(data)
+var = np.var(data)
+
+# PERCENTILES
+# I percentili vengono utilizzati in statistica per fornire un numero che descrive il valore rispetto
+# al quale una determinata percentuale dei valori è inferiore; X è il 75 percentile--> il 75% dei dati sono
+# inferiori o uguali a X
+data = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
+perc_75 = numpy.percentile(data, 75)
+
+
+
+
+
+
+
+
 
 
 
