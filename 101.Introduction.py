1a3e03bd41c41048a645dae1c69e596cf9dc001e
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-07 16:25:20 +0100

    Update and rename 101.Statistics_Basic.py to 101.Introduction.py

--- 101.Introduction.py
+++ 101.Introduction.py
@@ -17,6 +17,9 @@ var = np.var(data)
 data = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
 perc_75 = numpy.percentile(data, 75)
 
+# Per creare dataset grandi per prova o per testing usiamo 'random.uniform()'
+# X random floats tra a and b
+test = np.random.uniform(a, b, X)
 
 
 
