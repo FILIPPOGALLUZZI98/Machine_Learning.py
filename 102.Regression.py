48f0f74d4571d6cabf12cfc766fb52ebad431600
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-07 17:02:32 +0100

    Update 102.Regression.py

--- 102.Regression.py
+++ 102.Regression.py
@@ -1,6 +1,3 @@
-# Il termine regressione si usa quando vogliamo trovare la relazione tra variabili
-
-
 # LINEAR REGRESSION
 # Supponiamo di avere i seguenti array di dati (di uguale lunghezza)
 x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
@@ -9,14 +6,39 @@ plt.scatter(x, y); plt.show()
 
 # Per tracciare la retta di regressione
 slope, intercept, r, p, std_err = st.linregress(x, y)
-def myfunc(x):
+print(r)
+def reg_lin(x):
   return slope * x + intercept
-mymodel = list(map(myfunc, x))
-plt.scatter(x, y)
-plt.plot(x, mymodel)
-plt.show()
+mymodel = list(map(reg_lin, x))  ## This will result in a new array with new values for the y-axis
+plt.scatter(x, y); plt.plot(x, mymodel); plt.show()
 
-# 
+# Con la funzione creata possiamo anche predire i valori dei dati che non abbiamo
+predict = reg_lin(10); print(predict)
+
+
+
+
+# POLYNOMIAL REGRESSION
+from sklearn.metrics import r2_score 
+# Partiamo con due dataset
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+plt.scatter(x, y); plt.show()
+
+poly = np.poly1d(np.polyfit(x, y, 3))
+myline = np.linspace(1, 22, 100)  ## Indichiamo la posizione di inizio e fine della linea
+plt.scatter(x, y); plt.plot(myline, poly(my  line)); plt.show()
+print(r2_score(y, poly(x)))
+
+# Anche in questo caso possiamo predire i valori dei dati che non abbiamo
+predict = poly(17); print(predict)
+
+
+
+
+# MULTIPLE REGRESSION
+
+
 
 
 
