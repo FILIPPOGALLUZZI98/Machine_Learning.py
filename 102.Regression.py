0da5305a4574a6901a79f03c7df13d4179b7c0d0
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-07 16:44:01 +0100

    Update 102.Regression.py

--- 102.Regression.py
+++ 102.Regression.py
@@ -1,12 +1,22 @@
 # Il termine regressione si usa quando vogliamo trovare la relazione tra variabili
 
+
 # LINEAR REGRESSION
 # Supponiamo di avere i seguenti array di dati (di uguale lunghezza)
 x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
 y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 plt.scatter(x, y); plt.show()
 
+# Per tracciare la retta di regressione
+slope, intercept, r, p, std_err = st.linregress(x, y)
+def myfunc(x):
+  return slope * x + intercept
+mymodel = list(map(myfunc, x))
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()
 
+# 
 
 
 
