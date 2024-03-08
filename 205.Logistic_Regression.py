77c253feedfb1cc96232ac4922ada45196b66e58
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-08 13:56:51 +0100

    Update 205.Logistic_Regression.py

--- 205.Logistic_Regression.py
+++ 205.Logistic_Regression.py
@@ -9,17 +9,30 @@ from sklearn import linear_model
 # Esempio basato sui tumori
 # X rappresenta la grandezza di un tumore in centimetri
 X = np.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
-# X deve essere formattato in una colonna 
-from a row for the LogisticRegression() function to work
-# y represents whether or not the tumor is cancerous (0 for "No", 1 for "Yes").
-y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+# X deve essere formattato in una colonna da una riga per 'LogisticRegression()' 
+# y rappresenta se un tumore è maligno o no (0 per no, 1 per si)
+y = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
+# LogisticRegression() ha un metodo chiamato 'fit()' che prende valori indipendenti e dipendenti come parametri e riempie l'oggetto 
+# di regressione con i dati che descrivono la relazione
+logr = linear_model.LogisticRegression()
+logr.fit(X,y)
 
+# Per predire se un tumore è maligno o meno
+predicted = logr.predict(np.array([3.46]).reshape(-1,1))
 
+# I coefficienti ci danno molte informazioni
+log_odds = logr.coef_
+odds = np.exp(log_odds); odds
+# Per esempio in questo caso ci dice che se il tumore aumenta di 1mm, la sua probabilità di esseree maligno è 4x
 
-
-
-
+# Il coefficiente e l'intercetta possono essere usati per trovare la probabilità che ciascun tumore sia maligno
+def logit2prob(logr,x):
+  log_odds = logr.coef_ * x + logr.intercept_
+  odds = np.exp(log_odds)
+  probability = odds / (1 + odds)
+  return(probability)
+print(logit2prob(logr, X))
 
 
 
