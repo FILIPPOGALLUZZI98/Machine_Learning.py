bd46092d681c56ed52a0944f711039610618d187
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-11 11:39:10 +0100

    Update and rename 302.Neural_Network.py to 301.Neural_Network.py

---
+++ Tutorial_Vari/301.Neural_Network.py
@@ -0,0 +1,46 @@
+# Questi sono input unici dati ad ogni nuerone (supponiamo di averne 3) (per ora sono arbitrari)
+# Gli input in un neurone potrebbero arrivare sia da valori esterni che da un altro neurone precedente
+# Stiamo considerando 3 input in un solo neuronen, quindi avremo un solo bias
+inputs = [1, 2, 3, 2.5]  
+weights = [0.2, 0.8, -0.5, 1]
+bias = 2
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] +  inputs[3]*weights[3] + bias
+print(output)
+
+# Passiamo ora a 3 neuroni con 4 input (e quindi 3 output)
+inputs = [1, 2, 3, 2.5]
+# Ci saranno 3 set di pesi ed ognuno di essi avrà 4 valori più 3 bias
+weights1 = [0.2, 0.8, -0.5, 1]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] +  inputs[3]*weights1[3] + bias1,
+          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] +  inputs[3]*weights2[3] + bias2,
+          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] +  inputs[3]*weights3[3] + bias3]
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
