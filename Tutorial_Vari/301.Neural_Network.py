559f314fed5df6138c6a8101fb4cb37327a6dd33
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-11 13:11:29 +0100

    Update 301.Neural_Network.py

--- Tutorial_Vari/301.Neural_Network.py
+++ Tutorial_Vari/301.Neural_Network.py
@@ -50,11 +50,39 @@ print(output)
 
 
 # BATCH
-NUMERO 4 MINUTO 8
+import numpy as np
+inputs = [[1, 2, 3, 2.5], 
+           [2, 5, -1, 2], 
+           [-1.5, 2.7, 3.3, -0.8]]
+weights = [[0.2, 0.8, -0.5, 1], 
+           [0.5, -0.91, 0.26, -0.5], 
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
+# Dobbiamo usare la trasposta in questo caso, altrimenti non torna
+output = np.dot(inputs, np.array(weights).T) + biases  ## In questo caso abbiamo invertito inputs e weights
+
+# Possiamo anche usare due layer in modo che il primo sia input del secondo
+inputs = [[1, 2, 3, 2.5], 
+           [2, 5, -1, 2], 
+           [-1.5, 2.7, 3.3, -0.8]]
+weights1 = [[0.2, 0.8, -0.5, 1], 
+           [0.5, -0.91, 0.26, -0.5], 
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
+weights2 = [[0.1, -0.14, 0.5], 
+           [-0.5, 0.12, -0.33], 
+           [-0.44, 0.73, -0.13]]
+biases2 = [-1, 2, -0.5]
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+print(layer2_outputs)
 
 
-
-
+# Invece di scrivere molti layer possiamo usare una classe
+X = [[1, 2, 3, 2.5], 
+     [2, 5, -1, 2], 
+     [-1.5, 2.7, 3.3, -0.8]]
+# Definiamo gli hidden layer
 
 
 
