29da72e819bb1c32ed88c4ee49ba0b38abbc69b3
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-11 15:33:58 +0100

    Update 301.Neural_Network.py

--- Tutorial_Vari/301.Neural_Network.py
+++ Tutorial_Vari/301.Neural_Network.py
@@ -19,6 +19,7 @@ weights3 = [-0.26, -0.27, 0.17, 0.87]
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
+
 output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] +  inputs[3]*weights1[3] + bias1,
           inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] +  inputs[3]*weights2[3] + bias2,
           inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] +  inputs[3]*weights3[3] + bias3]
@@ -30,6 +31,7 @@ weights = [[0.2, 0.8, -0.5, 1],
            [0.5, -0.91, 0.26, -0.5], 
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
+
 layer_outputs = []
 for neuron_weights, neuron_bias in zip(weights, biases):
           neuron_output = 0
@@ -62,6 +64,7 @@ weights = [[0.2, 0.8, -0.5, 1],
            [0.5, -0.91, 0.26, -0.5], 
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
+
 # Dobbiamo usare la trasposta in questo caso, altrimenti non torna
 output = np.dot(inputs, np.array(weights).T) + biases  ## In questo caso abbiamo invertito inputs e weights
 
@@ -77,9 +80,13 @@ weights2 = [[0.1, -0.14, 0.5],
            [-0.5, 0.12, -0.33], 
            [-0.44, 0.73, -0.13]]
 biases2 = [-1, 2, -0.5]
+
 layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
 layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
 print(layer2_outputs)
+
+
+
 
 # Invece di scrivere molti layer possiamo usare una classe
 import numpy as np
@@ -88,6 +95,7 @@ np.random.seed(0)
 X = [[1, 2, 3, 2.5], 
      [2, 5, -1, 2], 
      [-1.5, 2.7, 3.3, -0.8]]
+
 # Definiamo gli hidden layer
 class Layer_Dense:
           def __init__(self, n_inputs, n_neurons):
@@ -124,6 +132,7 @@ class Activation_ReLU:
 
 
 
+# ??????
 import numpy as np
 # Supponiamo che i seguenti siano i valori di output
 layer_outputs = [4.8, 1.21, 2.385]
@@ -164,10 +173,10 @@ class Layer_Dense:
 class Activation_ReLU:
           def forward(self, inputs):
                     self.output = np.maximum(0, inputs)
-layer1 = Layer_Dense(4, 5)
-activation1.forward(X)
+layer1 = Layer_Dense(2, 5)
+activation1.Activation_ReLU()
 layer1.forward(X)
-activation.orward(layer1.output)
+activation1.forward(layer1.output)
 print(activation1.output)
 
 
