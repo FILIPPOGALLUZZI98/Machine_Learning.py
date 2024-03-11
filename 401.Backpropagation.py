b787077c727cbbe5e1af942fae1295b5bd6f6105
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-11 09:32:15 +0100

    Update 401.Backpropagation.py

--- 401.Backpropagation.py
+++ 401.Backpropagation.py
@@ -1,3 +1,20 @@
+# L'algoritmo di backpropagation è un supervised learning method per multilayer feed-forward networks
+# Il principio della backpropagation è di modellizzare una certa funzione modificandone i pesi interni dei segnali in input
+# Il training del sistema viene fatto usando un supervised learning method, dove gli errori tra l'output del sistema e quello
+# conosciuto è presentato dal sistema e usato per modificareil suo stato interno
+
+
+# Backpropagation può essere usato sia per classificazione che regressione
+
+
+
+
+# L'algoritmo di backpropagation consiste in due fasi:
+# 1) Il forward pass, in cui i nostri input vengono fatti passare attraverso la rete e le previsioni di
+# output ottenute (noto anche come fase di propagazione)
+# 2) Il backward pass, in cui calcoliamo il gradiente della funzione di perdita sullo strato finale (ovvero, 
+# lo strato di previsione) della rete e utilizziamo questo gradiente per applicare ricorsivamente la regola della
+# catena per aggiornare i pesi nella nostra rete (nota anche come fase di aggiornamento del peso)
 
 
 
