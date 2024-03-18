e241ed9e58b1546ef1686c6543cb62dd75d10858
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-18 15:25:07 +0100

    Update 102.???.py

--- Libro/102.???.py
+++ Libro/102.???.py
@@ -1,6 +1,6 @@
-
-
-
+# There are two major types of supervised machine learning problems, called classification and regression
+# In classification, the goal is to predict a class label, which is a choice from a predefined list of possibilities
+# For regression tasks, the goal is to predict a continuous number, or a floating-point number in programming terms
 
 
 
