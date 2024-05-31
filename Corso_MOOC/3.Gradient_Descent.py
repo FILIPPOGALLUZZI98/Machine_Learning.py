33ce048c7c7baa977824237e1b776b77545ea9d7
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-05-31 17:01:19 +0200

    Update and rename 3.Gradient_Descent to 3.Gradient_Descent.py

--- Corso_MOOC/3.Gradient_Descent.py
+++ Corso_MOOC/3.Gradient_Descent.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Consideriamo i seguenti dati
-x_train = np.array([1.0, 2.0])   #features
+x_train = np.array([1.0, 2.0])
 y_train = np.array([300.0, 500.0])
 
 
