3ff58e81ac9ba747260fef85b4f542d3856a7f98
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-05-31 18:25:35 +0200

    Update 2.Linear_Regression_Multiple.py

--- Linear_Regression_&_Classification/2.Linear_Regression_Multiple.py
+++ Linear_Regression_&_Classification/2.Linear_Regression_Multiple.py
@@ -28,14 +28,14 @@ f_wb = predict(x_vec, w_init, b_init)
 #### COST FUNCTION
 
 # Per fare la Cost Function in più dimensioni
-def compute_cost(X, y, w, b): 
+def cost(X, y, w, b): 
     m = X.shape[0]
     cost = 0.0
     for i in range(m):                                
         f_wb_i = np.dot(X[i], w) + b    
         cost = cost + (f_wb_i - y[i])**2     
     cost = cost / (2 * m)                      
-    return compute_cost
+    return cost
 
 
 #############################################################################################
