8e80bb9f6d64239adcff3d5b4a30147dd031e7bf
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-04 17:18:06 +0200

    Update 5.Logistic_Regression.py

--- Regression_&_Classification/5.Logistic_Regression.py
+++ Regression_&_Classification/5.Logistic_Regression.py
@@ -2,3 +2,18 @@
 def sigmoid(z):
     g = 1/(1+np.exp(-z))   
     return g
+
+# Cost Function for Logistic Regression
+def cost_logistic(X, y, w, b):
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+    cost = cost / m
+    return cost
+
+
+
+
