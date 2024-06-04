4abd19e9a87f1a009ce4da51d61cc53dd1c6b878
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-04 18:37:35 +0200

    Update 2.Linear_Regression_Multiple.py

--- Regression_&_Classification/2.Linear_Regression_Multiple.py
+++ Regression_&_Classification/2.Linear_Regression_Multiple.py
@@ -2,49 +2,46 @@ import copy, math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# In questo caso usiamo la vettorizzazione 
 # Il modello sarà del tipo f_wb = w_1*x_1 + w_2*x_2 + ... + b
 
-# Dataset (che contiene: area, # camere, # piani, età della casa)
+# Consideriamo il seguente dataset
 X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
-y_train = np.array([460, 232, 178])  ## Prezzo
+y_train = np.array([460, 232, 178]) 
+
+#############################################################################################
+#############################################################################################
+####  MODELLO
 
 # Impostiamo i valori dei parametri
 b_init = 785.1811367994083
 w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
-
-# Impostiamo la funzione per fare la previsione (ovvero il modello, usando la vettorizzazione)
-def predict(x, w, b): 
+def model(x, w, b): 
     p = np.dot(x, w) + b     
     return p  ## p -> previsione
 
-# Per fare una previsione
+# Per fare una previsione, selezioniamo un nuovo vettore
 x_vec = np.array([[2000, 3, 2, 10]])
-f_wb = predict(x_vec, w_init, b_init)
+f_wb = model(x_vec, w_init, b_init)
 
 
 #############################################################################################
 #############################################################################################
-#### COST FUNCTION
+####  GRADIENT DESCENT ALGORITHM
 
 # Per fare la Cost Function in più dimensioni
-def cost(X, y, w, b): 
+def cost_function(X, y, w, b): 
     m = X.shape[0]
     cost = 0.0
     for i in range(m):                                
         f_wb_i = np.dot(X[i], w) + b    
-        cost = cost + (f_wb_i - y[i])**2     
-    cost = cost / (2 * m)                      
+        cost_temp = cost + (f_wb_i - y[i])**2     
+    cost = cost_temp / (2 * m)                      
     return cost
 
 
-#############################################################################################
-#############################################################################################
-#### GRADIENT DESCENT
-
 # Per fare il gradiente in più dimensioni
-def compute_gradient(X, y, w, b): 
-    m,n = X.shape           #(number of examples, number of features)
+def gradient(X, y, w, b): 
+    m,n = X.shape  ## (number of examples, number of features)
     dj_dw = np.zeros((n,))
     dj_db = 0.
     for i in range(m):                             
@@ -58,20 +55,20 @@ def compute_gradient(X, y, w, b):
 
 
 # Per fare il GD in più dimensioni
-def gradient_descent(X, y, w_in, b_in, cost, compute_gradient, alpha, num_iters): 
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient, alpha, num_iters): 
     # An array to store cost J and w's at each iteration primarily for graphing later
     J_history = []
-    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    w = copy.deepcopy(w_in)  ## avoid modifying global w within function
     b = b_in
     for i in range(num_iters):
         # Calculate the gradient and update the parameters
-        dj_db,dj_dw = compute_gradient(X, y, w, b)   ##None
+        dj_db,dj_dw = gradient(X, y, w, b)   
         # Update Parameters using w, b, alpha and gradient
-        w = w - alpha * dj_dw               ##None
-        b = b - alpha * dj_db               ##None  
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db                 
         # Save cost J at each iteration
         if i<100000:      # prevent resource exhaustion 
-            J_history.append( cost(X, y, w, b))
+            J_history.append(cost_function(X, y, w, b))
         # Print cost every at intervals 10 times or as many iterations if < 10
         if i% math.ceil(num_iters / 10) == 0:
             print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")     
@@ -81,11 +78,11 @@ def gradient_descent(X, y, w_in, b_in, cost, compute_gradient, alpha, num_iters)
 # Per usare il GD
 initial_w = np.zeros_like(w_init)
 initial_b = 0.
-iterations = 1000
-alpha = 5.0e-7
+iterations = 100000
+alpha = 5.0e-11
 # Run gradient descent 
 w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b,
-                                                    cost, compute_gradient, 
+                                                    cost_function, gradient, 
                                                     alpha, iterations)
 print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
 m,_ = X_train.shape
