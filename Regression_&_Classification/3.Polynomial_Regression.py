f7df06083658458f1fcbe8c356e526b710fed073
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-07 16:51:01 +0200

    Update 3.Polynomial_Regression.py

--- Regression_&_Classification/3.Polynomial_Regression.py
+++ Regression_&_Classification/3.Polynomial_Regression.py
@@ -8,8 +8,7 @@ import matplotlib.pyplot as plt
 
 # Supponiamo di voler usare come modello f = 1+x^2
 x = np.arange(1, 16)
-y = 1 + x**2
-
+y = [4.9,7.8,14.7,27.6,28.5,41.4,53.3,69.2,90.1,120. ,135.9, 168.8,180.7,210.6,250.5]
 # Facciamo il GD algorithm cercando una relazione lineare
 w_in = 0; b_in = 0; alpha = 0.001; N = 100000
 model_w,model_b = gradient_descent_single(x,y,w_in,b_in, alpha, N ,cost_function_single, gradient_single)
