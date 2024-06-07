04977490edcba967439bb492c3aee1d2a7387f59
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-07 16:20:43 +0200

    Update 2.Linear_Regression_Multiple.py

--- Regression_&_Classification/2.Linear_Regression_Multiple.py
+++ Regression_&_Classification/2.Linear_Regression_Multiple.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178]) 
 
+
 #############################################################################################
 #############################################################################################
 ####  MODELLO
@@ -15,6 +16,8 @@ y_train = np.array([460, 232, 178])
 # Impostiamo i valori dei parametri
 b_init = 785.1811367994083
 w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+
+# Funzione che definisce il modello (e quindi la previsione)
 def model(x, w, b): 
     p = np.dot(x, w) + b     
     return p  ## p -> previsione
@@ -38,7 +41,6 @@ def cost_function(X, y, w, b):
     cost = cost_temp / (2 * m)                      
     return cost
 
-
 # Per fare il gradiente in più dimensioni
 def gradient(X, y, w, b): 
     m,n = X.shape  ## (number of examples, number of features)
@@ -53,11 +55,8 @@ def gradient(X, y, w, b):
     dj_db = dj_db / m                                
     return dj_db, dj_dw
 
-
 # Per fare il GD in più dimensioni
 def gradient_descent(X, y, w_in, b_in, cost_function, gradient, alpha, num_iters): 
-    # An array to store cost J and w's at each iteration primarily for graphing later
-    J_history = []
     w = copy.deepcopy(w_in)  ## avoid modifying global w within function
     b = b_in
     for i in range(num_iters):
@@ -66,24 +65,17 @@ def gradient_descent(X, y, w_in, b_in, cost_function, gradient, alpha, num_iters
         # Update Parameters using w, b, alpha and gradient
         w = w - alpha * dj_dw               
         b = b - alpha * dj_db                 
-        # Save cost J at each iteration
-        if i<100000:      # prevent resource exhaustion 
-            J_history.append(cost_function(X, y, w, b))
-        # Print cost every at intervals 10 times or as many iterations if < 10
-        if i% math.ceil(num_iters / 10) == 0:
-            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")     
-    return w, b, J_history #return final w,b and J history for graphing
+    return w, b 
+# Questo codice si può modificare per tenere conto di J ad ogni passaggio se si vuole
+# creare la learning curve (#iterations-J)
+    
 
-
-# Per usare il GD
-initial_w = np.zeros_like(w_init)
-initial_b = 0.
-iterations = 100000
-alpha = 5.0e-11
+# Per usare il GD bisogna definire w, b, alpha e il numero di iterazioni
+initial_w = np.zeros_like(w_init); initial_b = 0.; iterations = 10000; alpha = 5.0e-11
 # Run gradient descent 
-w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b,
-                                                    cost_function, gradient, 
-                                                    alpha, iterations)
+w_final, b_final = gradient_descent(X_train, y_train, initial_w, initial_b, cost_function,
+                                    gradient, alpha, iterations)
+
 print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
 m,_ = X_train.shape
 for i in range(m):
