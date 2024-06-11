ac7d309e962de763b636f37db744d18e9e49bccc
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-11 16:09:41 +0200

    Update 5.Softmax_Regression.py

--- Neural_Networks/5.Softmax_Regression.py
+++ Neural_Networks/5.Softmax_Regression.py
@@ -5,6 +5,11 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
+
+#############################################################################################
+#############################################################################################
+####  MODEL IMPLEMENTATION
+
 # In questo caso usiamo l'esempio di handwritten digit recognition (quindi con 
 # l'output = 10 cifre)
 
@@ -16,9 +21,29 @@ model = Sequential([
 
 # Specificare loss and cost
 model.compile(loss= SparseCategoricalCrossentropy())
+## Se vogliamo rendere l'algoritmo più accurato (cioè ridurre lo spazio di memoria di lavoro
+## e quindi renderlo più veloce e ridurre i numerical round-off errors):
+# model.compile(loss= SparseCategoricalCrossentropy(from_logits=True))
 
 # Train on data to minimize the loss function
 model.fit(X,Y, epochs=100)
+## Se usiamo il modello improved allora:
+# logit = model(X)
+# f_x = ft.nn.sigmoid(logit)  ## Restituisce le z_j
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
