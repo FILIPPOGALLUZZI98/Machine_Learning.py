3a1b8665b7e5bf9a9a60a391d49632a51b19aebf
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-17 16:45:15 +0200

    Create 4.XGBoost.py

---
+++ Decision_Trees/4.XGBoost.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+import matplotlib.pyplot as plt
+
+# Let's define a subset of our training set (we should not use the test set here).
+n = int(len(X_train)*0.8) ## Let's use 80% to train and 20% to eval
+X_train_fit, X_train_eval, y_train_fit, y_train_eval = X_train[:n], X_train[n:], y_train[:n], y_train[n:]
+
+# Model
+xgb_model = XGBClassifier(n_estimators = 500, learning_rate = 0.1,verbosity = 1, random_state = RANDOM_STATE)
+xgb_model.fit(X_train_fit,y_train_fit, eval_set = [(X_train_eval,y_train_eval)], early_stopping_rounds = 10)
+xgb_model.best_iteration
+print(f"Metrics train:\n\tAccuracy score: {accuracy_score(xgb_model.predict(X_train),y_train):.4f}\nMetrics test:\n\tAccuracy score: {accuracy_score(xgb_model.predict(X_val),y_val):.4f}")
+
+
+
+
+
+
+
+
+
+
+
