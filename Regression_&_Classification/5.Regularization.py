fd9fb739108d36dad42ccf1d8d9730bf8156abdc
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-21 16:27:17 +0200

    Update 5.Regularization.py

--- Regression_&_Classification/5.Regularization.py
+++ Regression_&_Classification/5.Regularization.py
@@ -8,42 +8,6 @@ import math, copy
 # La funzione del GD rimane invariata a quella dei file '2.Linear_Regression_Multiple.py' e
 # '4.Logistic_Regression.py'
 
-#############################################################################################
-#############################################################################################
-####  LINEAR REGRESSION
-
-# Algorithm for computing the regularized cost 
-def cost_regularized_linear(X, y, w, b, lambda_ = 1):
-    m  = X.shape[0]
-    n  = len(w)
-    cost = 0.
-    for i in range(m):
-        f_wb_i = np.dot(X[i], w) + b                                   
-        cost = cost + (f_wb_i - y[i])**2                                            
-    cost = cost / (2 * m)                                                
-    reg_cost = 0
-    for j in range(n):
-        reg_cost += (w[j]**2)                                          
-    reg_cost = (lambda_/(2*m)) * reg_cost                              
-    total_cost = cost + reg_cost                                       
-    return total_cost  
-    
-# Algorithm for computing the regularized gradient
-def gradient_regularized_linear(X, y, w, b, lambda_): 
-    m,n = X.shape           
-    dj_dw = np.zeros((n,))
-    dj_db = 0.
-    for i in range(m):                             
-        err = (np.dot(X[i], w) + b) - y[i]                 
-        for j in range(n):                         
-            dj_dw[j] = dj_dw[j] + err * X[i, j]               
-        dj_db = dj_db + err                        
-    dj_dw = dj_dw / m                                
-    dj_db = dj_db / m   
-    for j in range(n):
-        dj_dw[j] = dj_dw[j] + (lambda_/m) * w[j]
-    return dj_db, dj_dw
-
 
 #############################################################################################
 #############################################################################################
