024899a48ef47981bf917add2410955882ad232e
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-21 15:42:05 +0200

    Update Mount_Drive_Colab.py

--- Mount_Drive_Colab.py
+++ Mount_Drive_Colab.py
@@ -1,12 +1,7 @@
 # Montaggi GDrive
 import os, sys
-import xarray as xr
 from google.colab import drive
 drivedir='/content/drive'
 drive.mount(drivedir)
 os.chdir(drivedir)
 datadir=drivedir+'/MyDrive/Esempi_ML_Github/'
-
-
-# Aprire dataset
-ds=xr.open_dataset(datadir+'nome_dataset')
