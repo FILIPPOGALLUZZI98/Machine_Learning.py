072930b4512818d2fef73fc977824251556afb5a
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-22 15:25:58 +0200

    Update 3.Polynomial_Regression.py

--- Regression_&_Classification/3.Polynomial_Regression.py
+++ Regression_&_Classification/3.Polynomial_Regression.py
@@ -8,9 +8,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_squared_error
 
-# Il modello sarà del tipo f_wb = w1*x1 + w2*x1^2 + w3*x2^(-1/2) + w4*x3^3 + ... + b
-# In questo caso dobbiamo usare la regolarizzazione per evitare l'overfitting
-
 # Supponiamo di dover prevedere la resa di un campo in base ad alcune variabili come
 # la quantità di fertilizzanti usati, di acqua e ore di sole
 # Usiamo solo queste 3 variabili (anche se nel dataset ce ne sono di più)
@@ -38,55 +35,17 @@ plt.tight_layout(); plt.show()
 
 #############################################################################################
 #############################################################################################
-####  MODEL IMPLEMENTATION
+####  MODEL SELECTION
 
+# Data split in 3: train, cross-validation e test 
 X = df[['fertilizer_used', 'water_irrigated', 'sunlight_hours']].values
 y = df['crop_yield'].values
-
-# Creiamo il modello polinomiale con regolarizzazione Ridge
-degree = 3  ## Grado del polinomio
-Lambda = 0.5  ## Parametro di regolarizzazione
-
-# Applichiamo il modello
-model = make_pipeline(PolynomialFeatures(degree), Ridge(alpha=Lambda))
-model.fit(X, y)
-
-# Creiamo i grafici delle linee di regressione
-plt.figure(figsize=(14, 10))
-for i, feature in enumerate(features):
-    plt.subplot(2, 2, i + 1)
-    plt.scatter(df[feature], df['crop_yield'], s=1, color='k', label='Dati')
-    x_vals = np.linspace(df[feature].min(), df[feature].max(), 100).reshape(-1, 1)
-    X_vals = np.full((x_vals.shape[0], X.shape[1]), df[features].mean(axis=0))
-    X_vals[:, i] = x_vals.flatten()
-    y_vals = model.predict(X_vals)
-    plt.plot(x_vals, y_vals, color='r', label='Regressione Polinomiale')
-    plt.title(titles[i]); plt.xlabel(feature); plt.ylabel('Crop Yield'); plt.legend()
-plt.tight_layout(); plt.show()
-
-# Per ottenere la formula del modello
-polynomial_features = model.named_steps['polynomialfeatures']
-ridge = model.named_steps['ridge']
-feature_names = polynomial_features.get_feature_names_out(features)
-
-# Otteniamo i coefficienti e l'intercetta
-coefficients = ridge.coef_; intercept = ridge.intercept_
-
-# Mostriamo la forma matematica del modello
-terms = [f"{coeff:.4f}*{name}" for coeff, name in zip(coefficients, feature_names)]
-formula = " + ".join(terms); formula = f"{intercept:.4f} + " + formula
-print("La forma matematica del modello è:"); print(formula)
-
-
-#############################################################################################
-#############################################################################################
-####  MODEL SELECTION
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)  ## 60-40
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)  ## 50-50
 
 Lambda = 0.5  ## Parametro di regolarizzazione
 degrees = range(1, 11)  ## Gradi del polinomio da testare
-mse_train_list = []  ## Lista per salvare gli MSE sul set di addestramento
-mse_cv_list = []  ## Lista per salvare gli MSE della cross-validation
-mse_test_list = []  ## Lista per salvare gli MSE sul set di test
+mse_train_list = []; mse_cv_list = []; mse_test_list = [] 
 
 # Testiamo diversi gradi del polinomio
 for degree in degrees:
@@ -111,6 +70,41 @@ plt.plot(degrees, mse_test_list, marker='o', linestyle='-', color='r', label='Te
 plt.title('MSE per Grado del Polinomio'); plt.xlabel('Grado del Polinomio')
 plt.ylabel('Mean Squared Error (MSE)'); plt.xticks(degrees); plt.legend()
 plt.grid(True); plt.show()
+
+
+#############################################################################################
+#############################################################################################
+####  MODEL IMPLEMENTATION
+
+# Creiamo il modello polinomiale con i migliori valori trovati
+degree = 3  ## Grado del polinomio
+Lambda = 0.5  ## Parametro di regolarizzazione
+
+# Applichiamo il modello
+model = make_pipeline(PolynomialFeatures(degree), Ridge(alpha=Lambda))
+model.fit(X_train, y_train)
+
+# Creiamo i grafici delle linee di regressione
+plt.figure(figsize=(14, 10))
+for i, feature in enumerate(features):
+    plt.subplot(2, 2, i + 1)
+    plt.scatter(df[feature], df['crop_yield'], s=1, color='k', label='Dati')
+    x_vals = np.linspace(df[feature].min(), df[feature].max(), 100).reshape(-1, 1)
+    X_vals = np.full((x_vals.shape[0], X.shape[1]), df[features].mean(axis=0))
+    X_vals[:, i] = x_vals.flatten()
+    y_vals = model.predict(X_vals)
+    plt.plot(x_vals, y_vals, color='r', label='Regressione Polinomiale')
+    plt.title(titles[i]); plt.xlabel(feature); plt.ylabel('Crop Yield'); plt.legend()
+plt.tight_layout(); plt.show()
+
+# Per ottenere la formula del modello
+polynomial_features = model.named_steps['polynomialfeatures']
+ridge = model.named_steps['ridge']
+feature_names = polynomial_features.get_feature_names_out(features)
+coefficients = ridge.coef_; intercept = ridge.intercept_
+terms = [f"{coeff:.4f}*{name}" for coeff, name in zip(coefficients, feature_names)]
+formula = " + ".join(terms); formula = f"{intercept:.4f} + " + formula
+print("La forma matematica del modello è:"); print(formula)
 
 
 
