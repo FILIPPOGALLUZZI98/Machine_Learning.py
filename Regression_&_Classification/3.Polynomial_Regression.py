5d1b4d334a36285a06163f5707c32f7befd4a284
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-22 14:41:18 +0200

    Update 3.Polynomial_Regression.py

--- Regression_&_Classification/3.Polynomial_Regression.py
+++ Regression_&_Classification/3.Polynomial_Regression.py
@@ -2,7 +2,9 @@ import copy, math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import make_pipeline
 
 # Il modello sarà del tipo f_wb = w1*x1 + w2*x1^2 + w3*x2^(-1/2) + w4*x3^3 + ... + b
 # In questo caso dobbiamo usare la regolarizzazione per evitare l'overfitting
@@ -11,7 +13,6 @@ import matplotlib.pyplot as plt
 # la quantità di fertilizzanti usati, di acqua e ore di sole
 # Usiamo solo queste 3 variabili (anche se nel dataset ce ne sono di più)
 
-# Alla fine c'è l'implementazione con 'scikit-learn'
 
 #############################################################################################
 #############################################################################################
@@ -35,127 +36,7 @@ plt.tight_layout(); plt.show()
 
 #############################################################################################
 #############################################################################################
-####  MODELLO
-
-# Supponiamo di volere il modello: f_wb = w1*x1 + w2*x1^3 + w3*x2 + w4*x2^2 + w5*x3 + w6*x3^2 + b
-# x1 -> fertilizer; x2 -> water; w3 -> sunlight
-
-# Costruiamo due engineered variables per ottenere: X = (x1, x1^3, x2, x2^2, x3, x3^2)
-# In questo caso creiamo 3 colonne aggiuntive al dataset df
-df['fertilizer_used_sq'] = df['fertilizer_used'] ** 3
-df['water_irrigated_sq'] = df['water_irrigated'] ** 2
-df['sunlight_hours_sq'] = df['sunlight_hours'] ** 2
-
-# Creiamo il vettore X con le colonne desiderate
-X = df[['fertilizer_used', 'fertilizer_used_sq', 'water_irrigated', 'water_irrigated_sq',
-        'sunlight_hours', 'sunlight_hours_sq']].values
-y = df['crop_yield'].values  ## Variabile dipendente
-
-
-# Il modello sarà quindi lo stesso che per la regressione lineare con più variabili
-def model(x, w, b): 
-    p = np.dot(x, w) + b     
-    return p 
-
-# Per fare una previsione, selezioniamo un nuovo vettore esempio
-X_test = np.array([2,8,3,16,12,144]); w_in = np.array([3,12,0.1,6, 7,0.5]); b_in = -2
-f_wb = model(X_test, w_in, b_in)
-
-
-#############################################################################################
-#############################################################################################
-####  GRADIENT DESCENT ALGORITHM
-
-# Algoritmo per calcolare la cost function regolarizzata
-def cost_function(X, y, w, b, lambda_):
-    m = X.shape[0]
-    n = len(w)
-    cost = 0.
-    for i in range(m):
-        f_wb_i = np.dot(X[i], w) + b
-        cost = cost + (f_wb_i - y[i])**2
-    cost = cost / (2 * m)
-    reg_cost = 0
-    for j in range(n):
-        reg_cost += (w[j]**2)
-    reg_cost = (lambda_/(2*m)) * reg_cost
-    total_cost = cost + reg_cost
-    return total_cost
-
-# Algoritmo per calcolare il gradiente regolarizzato
-def gradient(X, y, w, b, lambda_):
-    m, n = X.shape
-    k = len(w)
-    dj_dw = np.zeros((n,))
-    dj_db = 0.
-    for i in range(m):
-        err = (np.dot(X[i], w) + b) - y[i]
-        for j in range(n):
-            dj_dw[j] = dj_dw[j] + err * X[i, j]
-        dj_db = dj_db + err
-    dj_dw = dj_dw / m
-    dj_db = dj_db / m
-    for j in range(k):
-        dj_dw[j] = dj_dw[j] + (lambda_/m) * w[j]
-    return dj_db, dj_dw
-
-# Funzione per il gradiente discendente
-def gradient_descent(X, y, w_in, b_in, alpha, num_iters, lambda_):
-    J_history = []
-    w = copy.deepcopy(w_in)
-    b = b_in
-    for i in range(num_iters):
-        dj_db, dj_dw = gradient(X, y, w, b, lambda_)
-        w = w - alpha * dj_dw
-        b = b - alpha * dj_db
-        if i < 100000: 
-            J_history.append(cost_function(X, y, w, b, lambda_))
-        # Stampa il costo a intervalli di 10 volte o per il numero di iterazioni se < 10
-        if i % math.ceil(num_iters / 10) == 0:
-            print(f"Iteration {i:4d}: Cost {J_history[-1]}")
-    return w, b, J_history 
-
-
-#############################################################################################
-#############################################################################################
-####  APPLICARE GRADIENT DESCENT 
-
-# Facciamo i grafici per 4 valori di alpha (con lambda=0) e con 4 valori di lambda con il migliore alpha
-initial_w = np.zeros(X.shape[1]); initial_b = 0.; iterations = 10000
-
-alpha_values = [1e-15, 1e-16, 1e-17, 1e-18]
-plt.figure(figsize=(14, 10))
-for i, alpha in enumerate(alpha_values):
-    _,_,J_history = gradient_descent(X, y, initial_w, initial_b, alpha=alpha, num_iters=iterations, lambda_=0)
-    plt.subplot(2, 2, i + 1)
-    plt.plot(J_history); plt.title(f'Alpha = {alpha}'); plt.xlabel('Iterations'); plt.ylabel('Cost J')
-plt.tight_layout(); plt.show()
-
-lambda_values = [0.05, 0.1, 0.5, 1]
-alpha_scelto = 1e-15
-plt.figure(figsize=(14, 10))
-for i, lambda_ in enumerate(lambda_values):
-    _,_,J_history = gradient_descent(X, y, initial_w, initial_b, alpha=alpha_scelto, num_iters=iterations, lambda_=lambda_)
-    plt.subplot(2, 2, i + 1); plt.plot(J_history); plt.title(f'Lambda = {lambda_}'); plt.xlabel('Iterations'); plt.ylabel('Cost J')
-plt.tight_layout(); plt.show()
-
-
-# Facciamo ora il gradient descent con i migliori valori di alpha e lambda
-lambda_scelto = 0.5
-initial_w = np.zeros(X.shape[1]); initial_b = 0.; iterations = 1000000; alpha = alpha_scelto; lambda_ = lambda_scelto
-w_final, b_final, _ = gradient_descent(X, y, initial_w, initial_b, alpha, iterations, lambda_)
-
-
-
-
-#############################################################################################
-#############################################################################################
-####  SCIKIT-LEARN IMPLEMENTATION
-
-# Il codice è uguale fino a 'MODELLO'
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import Ridge
-from sklearn.pipeline import make_pipeline
+####  MODEL IMPLEMENTATION
 
 X = df[['fertilizer_used', 'water_irrigated', 'sunlight_hours']].values
 y = df['crop_yield'].values
@@ -195,15 +76,39 @@ formula = " + ".join(terms); formula = f"{intercept:.4f} + " + formula
 print("La forma matematica del modello è:"); print(formula)
 
 
+#############################################################################################
+#############################################################################################
+####  MODEL SELECTION
 
+Lambda = 0.5  ## Parametro di regolarizzazione
+degrees = range(1, 11)  ## Gradi del polinomio da testare
+mse_train_list = []  ## Lista per salvare gli MSE sul set di addestramento
+mse_cv_list = []  ## Lista per salvare gli MSE della cross-validation
+mse_test_list = []  ## Lista per salvare gli MSE sul set di test
 
+# Testiamo diversi gradi del polinomio
+for degree in degrees:
+    model = make_pipeline(PolynomialFeatures(degree), Ridge(alpha=Lambda))
+    model.fit(X_train, y_train)
+    y_train_pred = model.predict(X_train)
+    mse_train = mean_squared_error(y_train, y_train_pred)
+    mse_train_list.append(mse_train)
+    cv_scores = cross_val_score(model, X_train, y_train, scoring='neg_mean_squared_error', cv=5)
+    mse_cv = -np.mean(cv_scores)
+    mse_cv_list.append(mse_cv)
+    y_test_pred = model.predict(X_test)
+    mse_test = mean_squared_error(y_test, y_test_pred)
+    mse_test_list.append(mse_test)
+    print(f'Degree {degree}: Train MSE = {mse_train}, CV MSE = {mse_cv}, Test MSE = {mse_test}')
 
-
-
-
-
-
-
+# Visualizziamo gli MSE per ogni grado del polinomio
+plt.figure(figsize=(10, 10))
+plt.plot(degrees, mse_train_list, marker='o', linestyle='-', color='b', label='Train MSE')
+plt.plot(degrees, mse_cv_list, marker='o', linestyle='-', color='g', label='CV MSE')
+plt.plot(degrees, mse_test_list, marker='o', linestyle='-', color='r', label='Test MSE')
+plt.title('MSE per Grado del Polinomio'); plt.xlabel('Grado del Polinomio')
+plt.ylabel('Mean Squared Error (MSE)'); plt.xticks(degrees); plt.legend()
+plt.grid(True); plt.show()
 
 
 
