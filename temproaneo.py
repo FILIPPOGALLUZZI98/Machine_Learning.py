4be658ac8266a4388616f95d2b6d9ace4da514eb
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-06-22 12:22:19 +0200

    Delete temproaneo.py

--- temproaneo.py
+++
@@ -1,3 +0,0 @@
-from sklearn.model_selection import train_test_split
-# Dividiamo i dati in train(80%)/ test(20%)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
